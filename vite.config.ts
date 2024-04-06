a34c3ca0ab0916d644488dc730269028cad3e8a7
LonitLu <dimasik17021989@gmail.com>
2024-04-07 00:52:28 +0500

    dep

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://lonitlu.github.io/Users-catalog/",
   plugins: [react()],
-  base: "YOUR_REPOSITORY_NAME",
 });
